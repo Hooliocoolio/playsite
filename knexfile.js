b34f3e40cc4d37ceda83c8caebcfa45df79309c6
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-20 16:57:17 -0700

    updated knexfile

--- knexfile.js
+++ knexfile.js
@@ -21,16 +21,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      host: `${process.env.DB_HOST}`,
-      user: `${process.env.DB_USER}`,
-      password: `${process.env.DB_PASSWORD}`,
-      database: `${process.env.DB_NAME}`,
-      ssl: {
-        sslmode: 'require',
-        rejectUnauthorized: false,
-      },
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
     },
