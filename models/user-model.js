b9a1cbb005a4439a56d862f3dd731c8561f51f57
Hoolio_Osuna <hoolio.Osuna@gmail.com>
2020-09-20 17:52:59 -0700

    updated database

--- models/user-model.js
+++ models/user-model.js
@@ -1,42 +1,32 @@
-const db = require('../data/dbconfig')
-
+const db = require("../data/dbconfig");
+const uuid = require("uuid");
 
 function findById(id) {
-    return db('users')
-        .select('id', 'username')
-        .where({ id })
-        .first()
+  return db("users").select("id", "username").where({ id }).first();
 }
 
-
 async function addUser(user) {
-    const [id] = await db('users').insert(user)
-    return findById(id)
+  const id = uuid.v4();
+  await db("users").insert({ ...user, id });
+  return findById(id);
 }
 
 function allUsers() {
-    return db('users')
-      
+  return db("users");
 }
 
 function findUser(filter) {
-    return db('users')
-        .select('id', 'username', 'password', 'role')
-        .where(filter)
-} 
-
+  return db("users").select("id", "username", "password", "role").where(filter);
+}
 
 function updateUser(changes, id) {
-    return db('users')
-        .update(changes)
-        .where({ id })
+  return db("users").update(changes).where({ id });
 }
-
 
 module.exports = {
-    findById,
-    addUser,
-    allUsers,
-    findUser,
-    updateUser
-}
+  findById,
+  addUser,
+  allUsers,
+  findUser,
+  updateUser,
+};
